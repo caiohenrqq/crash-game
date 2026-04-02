587819aee1fae85b4b6d3a40e292f89baf352c7a
caiohenrqq <caiohenrqq@gmail.com>
2026-04-02 15:47:00 -0400

    build(api): wire throttling into games & wallets modules

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,40 +1,101 @@
 import { createNestPostgreSqlMikroOrmModule } from '@crash/foundation/database/postgresql-mikro-orm';
+import {
+	AuthenticatedPlayerThrottlerGuard,
+	createHttpThrottlerOptions,
+} from '@crash/foundation/nest/http-throttler';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CashOutBetUseCase } from './application/cash-out-bet.use-case';
+import {
+	ACTIVE_ROUND_PHASE_MS,
+	CurrentRoundMultiplierService,
+} from './application/current-round-multiplier.service';
 import { GetCurrentRoundUseCase } from './application/get-current-round.use-case';
+import { InMemorySettlementCompletionCoordinator } from './application/in-memory-settlement-completion-coordinator';
+import { PlaceBetUseCase } from './application/place-bet.use-case';
+import { BetRepository } from './application/ports/bet.repository';
 import { RoundRepository } from './application/ports/round.repository';
+import { SettlementCompletionNotifier } from './application/ports/settlement-completion-notifier';
+import { SettlementOperationRepository } from './application/ports/settlement-operation.repository';
+import { SettlementRequester } from './application/ports/settlement-requester';
+import { ProcessSettlementCompletionUseCase } from './application/process-settlement-completion.use-case';
 import { RoundLifecycleService } from './application/round-lifecycle.service';
 import { JoseTokenVerifier } from './infrastructure/auth/jose-token-verifier';
 import { JwtAuthenticationGuard } from './infrastructure/auth/jwt-authentication.guard';
 import { TOKEN_VERIFIER } from './infrastructure/auth/token-verifier';
+import { RabbitMqGamesSettlementBroker } from './infrastructure/broker/rabbit-mq-games-settlement-broker';
 import { gamesConfig } from './infrastructure/config/games-config';
 import { BetEntitySchema } from './infrastructure/database/entities/bet.entity';
 import { RoundEntitySchema } from './infrastructure/database/entities/round.entity';
+import { SettlementOperationEntitySchema } from './infrastructure/database/entities/settlement-operation.entity';
+import { MikroOrmBetRepository } from './infrastructure/database/mikro-orm-bet.repository';
 import { MikroOrmRoundRepository } from './infrastructure/database/mikro-orm-round.repository';
+import { MikroOrmSettlementOperationRepository } from './infrastructure/database/mikro-orm-settlement-operation.repository';
 import { GamesController } from './presentation/controllers/games.controller';
 
 @Module({
 	imports: [
+		ThrottlerModule.forRoot(createHttpThrottlerOptions(gamesConfig)),
 		createNestPostgreSqlMikroOrmModule({
 			databaseUrl: gamesConfig.databaseUrl,
-			entities: [RoundEntitySchema, BetEntitySchema],
+			entities: [
+				RoundEntitySchema,
+				BetEntitySchema,
+				SettlementOperationEntitySchema,
+			],
 			migrationsPath: `${process.cwd()}/src/infrastructure/database/migrations`,
 		}),
-		MikroOrmModule.forFeature([RoundEntitySchema, BetEntitySchema]),
+		MikroOrmModule.forFeature([
+			RoundEntitySchema,
+			BetEntitySchema,
+			SettlementOperationEntitySchema,
+		]),
 	],
 	controllers: [GamesController],
 	providers: [
 		GetCurrentRoundUseCase,
+		PlaceBetUseCase,
+		CashOutBetUseCase,
+		ProcessSettlementCompletionUseCase,
+		InMemorySettlementCompletionCoordinator,
+		CurrentRoundMultiplierService,
+		RabbitMqGamesSettlementBroker,
 		RoundLifecycleService,
+		AuthenticatedPlayerThrottlerGuard,
 		JwtAuthenticationGuard,
+		{
+			provide: ACTIVE_ROUND_PHASE_MS,
+			useValue: gamesConfig.activeRoundPhaseMs,
+		},
 		{
 			provide: APP_GUARD,
 			useExisting: JwtAuthenticationGuard,
 		},
 		{
+			provide: APP_GUARD,
+			useExisting: AuthenticatedPlayerThrottlerGuard,
+		},
+		{
 			provide: RoundRepository,
 			useClass: MikroOrmRoundRepository,
+		},
+		{
+			provide: BetRepository,
+			useClass: MikroOrmBetRepository,
+		},
+		{
+			provide: SettlementOperationRepository,
+			useClass: MikroOrmSettlementOperationRepository,
+		},
+		{
+			provide: SettlementCompletionNotifier,
+			useExisting: InMemorySettlementCompletionCoordinator,
+		},
+		{
+			provide: SettlementRequester,
+			useExisting: RabbitMqGamesSettlementBroker,
 		},
 		{
 			provide: TOKEN_VERIFIER,
@@ -47,7 +108,11 @@ import { GamesController } from './presentation/controllers/games.controller';
 	],
 })
 export class AppModule {
-	constructor(private readonly roundLifecycleService: RoundLifecycleService) {
+	constructor(
+		private readonly roundLifecycleService: RoundLifecycleService,
+		private readonly rabbitMqGamesSettlementBroker: RabbitMqGamesSettlementBroker,
+	) {
 		void this.roundLifecycleService;
+		void this.rabbitMqGamesSettlementBroker;
 	}
 }
--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,39 +1,69 @@
 import { createNestPostgreSqlMikroOrmModule } from '@crash/foundation/database/postgresql-mikro-orm';
+import {
+	AuthenticatedPlayerThrottlerGuard,
+	createHttpThrottlerOptions,
+} from '@crash/foundation/nest/http-throttler';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { CreateWalletUseCase } from './application/create-wallet.use-case';
 import { GetWalletUseCase } from './application/get-wallet.use-case';
+import { HandleSettlementRequestUseCase } from './application/handle-settlement-request.use-case';
+import { SettlementCompletionPublisher } from './application/ports/settlement-completion.publisher';
 import { WalletRepository } from './application/ports/wallet.repository';
+import { WalletSettlementOperationRepository } from './application/ports/wallet-settlement-operation.repository';
 import { JoseTokenVerifier } from './infrastructure/auth/jose-token-verifier';
 import { JwtAuthenticationGuard } from './infrastructure/auth/jwt-authentication.guard';
 import { TOKEN_VERIFIER } from './infrastructure/auth/token-verifier';
+import { RabbitMqWalletSettlementBroker } from './infrastructure/broker/rabbit-mq-wallet-settlement-broker';
 import { walletsConfig } from './infrastructure/config/wallets-config';
 import { WalletEntitySchema } from './infrastructure/database/entities/wallet.entity';
+import { WalletSettlementOperationEntitySchema } from './infrastructure/database/entities/wallet-settlement-operation.entity';
 import { MikroOrmWalletRepository } from './infrastructure/database/mikro-orm-wallet.repository';
+import { MikroOrmWalletSettlementOperationRepository } from './infrastructure/database/mikro-orm-wallet-settlement-operation.repository';
 import { WalletsController } from './presentation/controllers/wallets.controller';
 
 @Module({
 	imports: [
+		ThrottlerModule.forRoot(createHttpThrottlerOptions(walletsConfig)),
 		createNestPostgreSqlMikroOrmModule({
 			databaseUrl: walletsConfig.databaseUrl,
-			entities: [WalletEntitySchema],
+			entities: [WalletEntitySchema, WalletSettlementOperationEntitySchema],
 			migrationsPath: `${process.cwd()}/src/infrastructure/database/migrations`,
 		}),
-		MikroOrmModule.forFeature([WalletEntitySchema]),
+		MikroOrmModule.forFeature([
+			WalletEntitySchema,
+			WalletSettlementOperationEntitySchema,
+		]),
 	],
 	controllers: [WalletsController],
 	providers: [
 		CreateWalletUseCase,
 		GetWalletUseCase,
+		HandleSettlementRequestUseCase,
+		RabbitMqWalletSettlementBroker,
+		AuthenticatedPlayerThrottlerGuard,
 		JwtAuthenticationGuard,
 		{
 			provide: APP_GUARD,
 			useExisting: JwtAuthenticationGuard,
 		},
 		{
+			provide: APP_GUARD,
+			useExisting: AuthenticatedPlayerThrottlerGuard,
+		},
+		{
 			provide: WalletRepository,
 			useClass: MikroOrmWalletRepository,
+		},
+		{
+			provide: WalletSettlementOperationRepository,
+			useClass: MikroOrmWalletSettlementOperationRepository,
+		},
+		{
+			provide: SettlementCompletionPublisher,
+			useExisting: RabbitMqWalletSettlementBroker,
 		},
 		{
 			provide: TOKEN_VERIFIER,
@@ -45,4 +75,10 @@ import { WalletsController } from './presentation/controllers/wallets.controller
 		},
 	],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(
+		private readonly rabbitMqWalletSettlementBroker: RabbitMqWalletSettlementBroker,
+	) {
+		void this.rabbitMqWalletSettlementBroker;
+	}
+}
